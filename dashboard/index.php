e9c4bc1f104f86b1d7dd18b7f12a80d9ab51d654
Valmir da Silva <valmirdasilva1999@gmail.com>
2020-05-10 15:55:38 -0300

    .

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,7 +1,7 @@
 <?php
-include('../verifica_login.php');
-session_start();
 
+session_start();
+include('../verifica_login.php');
 ?>
 <h1>Olá,<?php echo $_SESSION['usuario']; ?></h1>
 <h2><a href="../logout.php">SAIR</a></h2>
